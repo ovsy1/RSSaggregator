597e0d0f287b5cb1f7c9f4a7948421dcd6110e8d
Ratmir Kalimulin <kalimulin.91@bk.ru>
2022-05-24 11:20:10 +0300

    change RU localization

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,20 +1,16 @@
 export default {
   translation: {
-    lng: 'ru',
-    feedback: {
-      successfullyLoaded: 'RSS успешно загружен',
-      alreadyExists: 'RSS уже существует',
-      fieldRequired: 'Не должно быть пустым',
-      urlNotValid: 'Ссылка должна быть валидным URL',
-      rssParsingError: 'Ресурс не содержит валидный RSS',
-      networkError: 'Ошибка сети',
+    feedbackMsg: {
+      validation: {
+        empty: 'Не должно быть пустым',
+        notValid: 'Ссылка должна быть валидным URL',
+        duplication: 'RSS уже существует',
+      },
+      processState: {
+        notValid: 'Ресурс не содержит валидный RSS',
+        networkError: 'Ошибка сети',
+        success: 'RSS успешно загружен',
+      },
     },
-    modal: {
-      readFull: 'Читать полностью',
-      close: 'Закрыть',
-    },
-    watchButton: 'Просмотр',
-    feeds: 'Фиды',
-    posts: 'Посты',
   },
 };
